265ec67c403185d3136e97898d3a478f84b8cffc
abhinand b s <abhinandbs1@gmail.com>
2020-02-22 13:17:58 +0530

    added coin method

--- Assets/Scripts/selectrobots.cs
+++ Assets/Scripts/selectrobots.cs
@@ -12,24 +12,109 @@ public class selectrobots : MonoBehaviour
     private int selected=0;
     public Text selectedtext;
     private int currentselected;
-   
-  
-   
+    public Text finalcoin;
+    private float high;
+
+
     void Start()
     {
         Time.timeScale = 1;
         camerapos.transform.position = new Vector3(0f, 94f, -614f);
        
         xpos = 0f;
-        
+        //PlayerPrefs.DeleteAll();
+        // PlayerPrefs.SetFloat("coinscore", 5000);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       currentselected = PlayerPrefs.GetInt("player",0);
-        if(selected == currentselected)
+        high = PlayerPrefs.GetFloat("coinscore", 0);
+
+        finalcoin.text = "" + high;
+
+        //player0 default
+        int player1 = PlayerPrefs.GetInt("player1", 0);
+        int player2 = PlayerPrefs.GetInt("player2", 0);
+        int player3 =  PlayerPrefs.GetInt("player3", 0);
+        int player4 = PlayerPrefs.GetInt("player4", 0);
+
+        if (selected == 0)
         {
+            changeselect();
+        }
+        else if(selected == 1)
+        {
+            if (player1 == 1) {
+
+                changeselect();
+            }
+            else
+            {
+                selectedtext.text = "500";
+                selectedtext.color = Color.yellow;
+            }
+        }else if (selected == 2)
+        {
+            if (player2 == 1)
+            {
+
+                changeselect();
+            }
+            else
+            {
+                selectedtext.text = "1000";
+                selectedtext.color = Color.yellow;
+            }
+        }
+        else if (selected == 3)
+        {
+            if (player3 == 1)
+            {
+
+                changeselect();
+            }
+            else
+            {
+                selectedtext.text = "2500";
+                selectedtext.color = Color.yellow;
+            }
+        }
+        else if (selected == 4)
+        {
+            if (player4 == 1)
+            {
+
+                changeselect();
+            }
+            else
+            {
+                selectedtext.text = "5000";
+                selectedtext.color = Color.yellow;
+            }
+        }
+        
+
+
+
+
+
+
+    }
+
+    public void changeselect()
+    {
+        currentselected = PlayerPrefs.GetInt("player", 0);
+
+
+
+
+        if (selected == currentselected)
+        {
+
             Color newColor = new Color(0f, 250f, 228f, 255f);
             selectedtext.text = "SELECTED";
             selectedtext.color = newColor;
@@ -39,7 +124,6 @@ public class selectrobots : MonoBehaviour
             selectedtext.text = "SELECT";
             selectedtext.color = Color.white;
         }
-
     }
 
     public void Next()
@@ -67,7 +151,99 @@ public class selectrobots : MonoBehaviour
     }
     public void Selected()
     {
-        PlayerPrefs.SetInt("player", selected);
+        int player1 = PlayerPrefs.GetInt("player1", 0);
+        int player2 = PlayerPrefs.GetInt("player2", 0);
+        int player3 = PlayerPrefs.GetInt("player3", 0);
+        int player4 = PlayerPrefs.GetInt("player4", 0);
+
+        float totalcoin = PlayerPrefs.GetFloat("coinscore", 0);
+
+        
+        if(selected == 0)
+        {
+            PlayerPrefs.SetInt("player", selected);
+        }else if (selected == 1) {
+
+            if(player1 == 0)
+            {
+                if(totalcoin >= 500)
+                {
+                    float newcoin = totalcoin - 500;
+                    
+                    PlayerPrefs.SetInt("player1", 1);
+                    PlayerPrefs.SetInt("player", selected);
+                    PlayerPrefs.SetFloat("coinscore",newcoin);
+                }
+                else
+                {
+                    
+                }  
+            }
+            else if(player1 ==1)
+            {
+                PlayerPrefs.SetInt("player", selected);
+
+            }
+           
+        }else if(selected == 2)
+        {
+            if (player2 == 0)
+            {
+                if (totalcoin >= 1000)
+                {
+                    float newcoin = totalcoin - 1000;
+
+                    PlayerPrefs.SetInt("player2", 1);
+                    PlayerPrefs.SetInt("player", selected);
+                    PlayerPrefs.SetFloat("coinscore", newcoin);
+                }
+            }
+            else if (player2 == 1)
+            {
+                PlayerPrefs.SetInt("player", selected);
+
+            }
+        }
+        else if (selected == 3)
+        {
+            if (player3 == 0)
+            {
+                if (totalcoin >= 2500)
+                {
+                    float newcoin = totalcoin - 2500;
+
+                    PlayerPrefs.SetInt("player3", 1);
+                    PlayerPrefs.SetInt("player", selected);
+                    PlayerPrefs.SetFloat("coinscore", newcoin);
+                }
+            }
+            else if (player3 == 1)
+            {
+                PlayerPrefs.SetInt("player", selected);
+
+            }
+        }
+        else if (selected == 4)
+        {
+            if (player4 == 0)
+            {
+                if (totalcoin >= 5000)
+                {
+                    float newcoin = totalcoin - 5000;
+
+                    PlayerPrefs.SetInt("player4", 1);
+                    PlayerPrefs.SetInt("player", selected);
+                    PlayerPrefs.SetFloat("coinscore", newcoin);
+                }
+            }
+            else if (player4 == 1)
+            {
+                PlayerPrefs.SetInt("player", selected);
+
+            }
+        }
+
+
 
         //SceneManager.LoadScene("welcome");
     }
